2eab4c52327fe01a4b5907c00731bf1c9bf16d47
Mason Vines <mason.vines@gmail.com>
2014-02-13 00:26:51 -0500

    Added the ability to stop playback. Start button now toggles.

--- src/badger/avalon/MainActivity.java
+++ src/badger/avalon/MainActivity.java
@@ -1,24 +1,32 @@
 package badger.avalon;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.media.SoundPool;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements
+		MediaPlayer.OnCompletionListener {
 	private static final String TAG = "MainActivity";
 
 	private static final int PAUSE = 500;
+	MediaPlayer mediaPlayer;
 
-	private SoundPool sounds;
+	private List<Integer> trackList;
+	private Iterator<Integer> trackIterator;
 
 	private boolean merlin = false;
 	private boolean percival = false;
@@ -34,6 +42,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		setVolumeControlStream(AudioManager.STREAM_MUSIC);
+		trackList = new ArrayList();
 	}
 
 	@Override
@@ -43,20 +53,22 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-
 	public void showGuinevereRules(View view) {
 		showRules(R.string.guinevere, R.string.guinevere_include,
-				R.string.guinevere_rules, R.string.guinevere_advantage, R.color.loyalBlue);
+				R.string.guinevere_rules, R.string.guinevere_advantage,
+				R.color.loyalBlue);
 	}
 
 	public void showGalahadRules(View view) {
 		showRules(R.string.galahad, R.string.galahad_include,
-				R.string.galahad_rules, R.string.galahad_advantage, R.color.loyalBlue);
+				R.string.galahad_rules, R.string.galahad_advantage,
+				R.color.loyalBlue);
 	}
 
 	public void showBedivereRules(View view) {
 		showRules(R.string.bedivere, R.string.bedivere_include,
-				R.string.bedivere_rules, R.string.bedivere_advantage, R.color.loyalBlue);
+				R.string.bedivere_rules, R.string.bedivere_advantage,
+				R.color.loyalBlue);
 	}
 
 	private void showRules(int titleId, int includeId, int rulesId,
@@ -69,105 +81,116 @@ public class MainActivity extends Activity {
 		// Inflate and set the layout for the dialog
 		// Pass null as the parent view because its going in the dialog layout
 		View dialogView = inflater.inflate(R.layout.rules_dialog, null);
-		builder.setView(dialogView)
-				.setPositiveButton(R.string.ok,
-						new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog, int id) {
-								dialog.dismiss();
-							}
-						});
+		builder.setView(dialogView).setPositiveButton(R.string.ok,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.dismiss();
+					}
+				});
 
-		
 		AlertDialog dialog = builder.create();
 
-		TextView titleView = (TextView)dialogView.findViewById(R.id.dialog_title);
+		TextView titleView = (TextView) dialogView
+				.findViewById(R.id.dialog_title);
 		titleView.setText(titleId);
 		titleView.setBackgroundResource(titleColorId);
-		
-		TextView includeView = (TextView)dialogView.findViewById(R.id.dialog_include);
-		includeView.setText(includeId);
-		
-		TextView rulesView = (TextView)dialogView.findViewById(R.id.dialog_rules);
-		rulesView.setText(rulesId);
-		
-		TextView advantageView = (TextView)dialogView.findViewById(R.id.dialog_advantage);
-		advantageView.setText(advantageId);
-		
-		dialog.show();
 
-		/*
-		 * AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		 * 
-		 * builder.setMessage(rulesId) .setTitle(titleId);
-		 * 
-		 * builder.setPositiveButton(R.string.ok, new
-		 * DialogInterface.OnClickListener() { public void
-		 * onClick(DialogInterface dialog, int id) { dialog.dismiss(); } });
-		 * 
-		 * AlertDialog dialog = builder.create(); dialog.show();
-		 */
+		TextView includeView = (TextView) dialogView
+				.findViewById(R.id.dialog_include);
+		includeView.setText(includeId);
+
+		TextView rulesView = (TextView) dialogView
+				.findViewById(R.id.dialog_rules);
+		rulesView.setText(rulesId);
+
+		TextView advantageView = (TextView) dialogView
+				.findViewById(R.id.dialog_advantage);
+		advantageView.setText(advantageId);
+
+		dialog.show();
 	}
 
 	public void start(View view) {
 		Log.d(TAG, "Starting!");
 
-		CheckBox merlinCheckBox = (CheckBox) findViewById(R.id.merlinCheckBox);
-		merlin = merlinCheckBox.isChecked();
+		if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+			Log.d(TAG, "Stopping!");
+			mediaPlayer.stop();
+			mediaPlayer.release();
 
-		CheckBox percivalCheckBox = (CheckBox) findViewById(R.id.percivalCheckBox);
-		percival = percivalCheckBox.isChecked();
+			Button startButton = (Button) findViewById(R.id.startButton);
+			startButton.setText(R.string.start);
+		} else {
+			Button startButton = (Button) findViewById(R.id.startButton);
+			startButton.setText(R.string.stop);
 
-		CheckBox galahadCheckBox = (CheckBox) findViewById(R.id.galahadCheckBox);
-		galahad = galahadCheckBox.isChecked();
+			CheckBox merlinCheckBox = (CheckBox) findViewById(R.id.merlinCheckBox);
+			merlin = merlinCheckBox.isChecked();
 
-		CheckBox guinevereCheckBox = (CheckBox) findViewById(R.id.guinevereCheckBox);
-		guinevere = guinevereCheckBox.isChecked();
+			CheckBox percivalCheckBox = (CheckBox) findViewById(R.id.percivalCheckBox);
+			percival = percivalCheckBox.isChecked();
 
-		CheckBox bedivereCheckBox = (CheckBox) findViewById(R.id.bedivereCheckBox);
-		bedivere = bedivereCheckBox.isChecked();
+			CheckBox galahadCheckBox = (CheckBox) findViewById(R.id.galahadCheckBox);
+			galahad = galahadCheckBox.isChecked();
 
-		CheckBox morgannaCheckBox = (CheckBox) findViewById(R.id.morgannaCheckBox);
-		morganna = morgannaCheckBox.isChecked();
+			CheckBox guinevereCheckBox = (CheckBox) findViewById(R.id.guinevereCheckBox);
+			guinevere = guinevereCheckBox.isChecked();
 
-		CheckBox mordredCheckBox = (CheckBox) findViewById(R.id.mordredCheckBox);
-		mordred = mordredCheckBox.isChecked();
+			CheckBox bedivereCheckBox = (CheckBox) findViewById(R.id.bedivereCheckBox);
+			bedivere = bedivereCheckBox.isChecked();
 
-		CheckBox oberonCheckBox = (CheckBox) findViewById(R.id.oberonCheckBox);
-		oberon = oberonCheckBox.isChecked();
+			CheckBox morgannaCheckBox = (CheckBox) findViewById(R.id.morgannaCheckBox);
+			morganna = morgannaCheckBox.isChecked();
 
-		CheckBox lancelotCheckBox = (CheckBox) findViewById(R.id.lancelotCheckBox);
-		lancelot = lancelotCheckBox.isChecked();
+			CheckBox mordredCheckBox = (CheckBox) findViewById(R.id.mordredCheckBox);
+			mordred = mordredCheckBox.isChecked();
 
-		playClip();
+			CheckBox oberonCheckBox = (CheckBox) findViewById(R.id.oberonCheckBox);
+			oberon = oberonCheckBox.isChecked();
+
+			CheckBox lancelotCheckBox = (CheckBox) findViewById(R.id.lancelotCheckBox);
+			lancelot = lancelotCheckBox.isChecked();
+
+			playClip();
+		}
 	}
 
 	private void playClip() {
-		playStartPhase();
-		playMinions();
-		playMerlin();
-		playPercival();
-		playLancelot();
-		playGuinevere();
-		playFinal();
+		trackList.clear();
+		addStartPhase();
+		addMinions();
+		addMerlin();
+		addPercival();
+		addLancelot();
+		addGuinevere();
+		addFinal();
+
+		playTracks();
 	}
 
-	private void playSound(int soundId) {
-		MediaPlayer player = MediaPlayer.create(MainActivity.this, soundId);
-		player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-
-			@Override
-			public void onCompletion(MediaPlayer mp) {
-				mp.release();
-			}
-		});
-
-		player.start();
-		while (player.isPlaying())
-			;
+	private void playTracks() {
+		trackIterator = trackList.iterator();
+		mediaPlayer = MediaPlayer.create(getApplicationContext(),
+				trackIterator.next());
+		mediaPlayer.setOnCompletionListener(this);
+		mediaPlayer.start();
 	}
 
-	private void playStartPhase() {
-		playSound(R.raw.start);
+	public void onCompletion(MediaPlayer player) {
+		mediaPlayer.release();
+		if (trackIterator.hasNext()) {
+			mediaPlayer = MediaPlayer.create(getApplicationContext(),
+					trackIterator.next());
+			mediaPlayer.setOnCompletionListener(this);
+			mediaPlayer.start();
+		} else {
+			Button startButton = (Button) findViewById(R.id.startButton);
+			startButton.setText(R.string.start);
+		}
+	}
+
+	private void addStartPhase() {
+		trackList.add(R.raw.start);
 	}
 
 	private void pause() {
@@ -178,83 +201,83 @@ public class MainActivity extends Activity {
 		}
 	}
 
-	private void playMinions() {
+	private void addMinions() {
 		if (oberon && lancelot) {
-			playSound(R.raw.minion_yes_oberon_yes_lancelot);
+			trackList.add(R.raw.minion_yes_oberon_yes_lancelot);
 		} else if (oberon && !lancelot) {
-			playSound(R.raw.minion_yes_oberon_no_lancelot);
+			trackList.add(R.raw.minion_yes_oberon_no_lancelot);
 		} else if (!oberon && lancelot) {
-			playSound(R.raw.minion_no_oberon_yes_lancelot);
+			trackList.add(R.raw.minion_no_oberon_yes_lancelot);
 		} else {
-			playSound(R.raw.minion_no_oberon_no_lancelot);
+			trackList.add(R.raw.minion_no_oberon_no_lancelot);
 		}
 
 		pause();
 
-		playSound(R.raw.minion_reset);
+		trackList.add(R.raw.minion_reset);
 
 		pause();
 	}
 
-	private void playMerlin() {
+	private void addMerlin() {
 		if (!merlin) {
 			return;
 		}
 
 		if (mordred) {
-			playSound(R.raw.merlin_yes_mordred);
+			trackList.add(R.raw.merlin_yes_mordred);
 		} else {
-			playSound(R.raw.merlin_no_mordred);
+			trackList.add(R.raw.merlin_no_mordred);
 		}
 
 		pause();
 
-		playSound(R.raw.merlin_reset);
+		trackList.add(R.raw.merlin_reset);
 
 		pause();
 	}
 
-	private void playPercival() {
+	private void addPercival() {
 		if (!percival) {
 			return;
 		}
 
 		if (morganna) {
-			playSound(R.raw.percival_yes_morganna);
+			trackList.add(R.raw.percival_yes_morganna);
 		} else {
-			playSound(R.raw.percival_no_morganna);
+			trackList.add(R.raw.percival_no_morganna);
 		}
 
 		pause();
 
-		playSound(R.raw.percival_reset);
+		trackList.add(R.raw.percival_reset);
 
 		pause();
 	}
 
-	private void playLancelot() {
+	private void addLancelot() {
 		if (!lancelot) {
 			return;
 		}
 
-		playSound(R.raw.lancelot);
+		trackList.add(R.raw.lancelot);
 
 		pause();
 
-		playSound(R.raw.lancelot_reset);
+		trackList.add(R.raw.lancelot_reset);
 
 		pause();
 	}
 
-	private void playGuinevere() {
+	private void addGuinevere() {
 		if (guinevere) {
-			playSound(R.raw.guinevere);
+			trackList.add(R.raw.guinevere);
 			pause();
 		}
 	}
 
-	private void playFinal() {
-		playSound(R.raw.final_reset);
+	private void addFinal() {
+		trackList.add(R.raw.final_reset);
 	}
 
 }
